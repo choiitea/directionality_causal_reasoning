8098aa21fedaf0e95213461bfd4f914dac905ee9
choiitea <rickywjchoi@gmail.com>
2022-04-14 00:36:08 -0400

    identical shape feature rep, can be inversed + static training w/ freq

---
+++ OrderRepresentation/condition1_static.ex
@@ -0,0 +1,36 @@
+#frequency 50:50
+#Red Square + Purple Diamond
+I:
+freq: 50
+(ShapeONE)
+1 0 0 0 0 0 0 1 0
+(Color)
+1 0 0 0 0 0 0
+(ShapeTWO)
+0 0 0 1 0 0 0 0 1
+T:
+(ShapeONEOutput)
+1 0 0 0 0 0 0 1 0
+(ColorOutput)
+1 0 0 0 0 0 0
+(ShapeTWOOutput)
+0 0 0 1 0 0 0 0 1
+;
+
+#Green Cylinder + Yellow Circle
+I:
+freq: 50
+(ShapeONE)
+0 1 0 0 0 0 0 1 0
+(Color)
+0 0 1 0 0 0 0
+(ShapeTWO)
+0 0 0 0 1 0 0 0 1
+T:
+(ShapeONEOutput)
+0 1 0 0 0 0 0 1 0
+(ColorOutput)
+0 0 1 0 0 0 0
+(ShapeTWOOutput)
+0 0 0 0 1 0 0 0 1
+;
