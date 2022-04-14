101d9c0acb4629d2d5124043ea4835928af13ff2
choiitea <rickywjchoi@gmail.com>
2022-04-14 10:37:29 -0400

    SOC test with order rep

---
+++ OrderRepresentation/condition1_SOCtest.ex
@@ -0,0 +1,38 @@
+#Square Cylinder Triangle Diamond Circle Heart Cross Base Feature
+# Blue Triangle + Purple Diamond [SOC consistent]
+I:
+(ShapeONE)
+0 0 1 0 0 0 0 1 0
+(Color)
+0 0 0 0 1 0 0
+(ShapeTWO)
+0 0 0 1 0 0 0 0 1
+T:
+(ShapeONEOutput)
+0 0 1 0 0 0 0 1 0
+(ColorOutput)
+0 0 0 0 1 0 0
+(ShapeTWOOutput)
+0 0 0 1 0 0 0 0 1
+(Conclusion)
+1
+;
+
+# Blue Triangle + Yellow Circle [SOC Inconsistent]
+I:
+(ShapeONE)
+0 0 1 0 0 0 0 1 0
+(Color)
+0 0 0 0 1 0 0
+(Feature)
+0 1 0 0 0
+T:
+(ShapeONEOutput)
+0 0 1 0 0 0 0 1 0
+(ColorOutput)
+0 0 0 0 1 0 0
+(ShapeTWOOutput)
+0 0 0 0 1 0 0 0 1
+(Conclusion)
+1
+;
