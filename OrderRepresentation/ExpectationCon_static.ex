5bfbc496430ee23a6d63f77a45806f9a537288dc
choiitea <rickywjchoi@gmail.com>
2022-04-14 11:02:29 -0400

    2 static learning - noExpectation (50/50) and Expectation (75/25)

---
+++ OrderRepresentation/ExpectationCon_static.ex
@@ -0,0 +1,72 @@
+#frequency 75:25
+#Red Square + Purple Diamond
+freq: 75
+I:
+(ShapeONE)
+1 0 0 0 0 0 0 1 0
+(Color)
+1 0 0 0 0 0 0
+(ShapeTWO)
+0 0 0 1 0 0 0 0 1
+T:
+(ShapeONEOutput)
+1 0 0 0 0 0 0 1 0
+(ColorOutput)
+1 0 0 0 0 0 0
+(ShapeTWOOutput)
+0 0 0 1 0 0 0 0 1
+;
+
+#Purple Diamond + Red Square
+freq: 25
+I:
+(ShapeONE)
+0 0 0 1 0 0 0 1 0
+(Color)
+1 0 0 0 0 0 0
+(ShapeTWO)
+1 0 0 0 0 0 0 0 1
+T:
+(ShapeONEOutput)
+0 0 0 1 0 0 0 1 0
+(ColorOutput)
+1 0 0 0 0 0 0
+(ShapeTWOOutput)
+1 0 0 0 0 0 0 0 1
+;
+
+#Green Cylinder + Yellow Circle
+freq: 75
+I:
+(ShapeONE)
+0 1 0 0 0 0 0 1 0
+(Color)
+0 0 1 0 0 0 0
+(ShapeTWO)
+0 0 0 0 1 0 0 0 1
+T:
+(ShapeONEOutput)
+0 1 0 0 0 0 0 1 0
+(ColorOutput)
+0 0 1 0 0 0 0
+(ShapeTWOOutput)
+0 0 0 0 1 0 0 0 1
+;
+
+#Yellow Circle + Green Cylinder
+freq: 25
+I:
+(ShapeONE)
+0 0 0 0 1 0 0 1 0
+(Color)
+0 0 1 0 0 0 0
+(ShapeTWO)
+0 1 0 0 0 0 0 0 1
+T:
+(ShapeONEOutput)
+0 0 0 0 1 0 0 1 0
+(ColorOutput)
+0 0 1 0 0 0 0
+(ShapeTWOOutput)
+0 1 0 0 0 0 0 0 1
+;
