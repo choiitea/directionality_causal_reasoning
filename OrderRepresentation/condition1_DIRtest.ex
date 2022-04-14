296aaf0e8efd8f195143ec79d7a1d4dd095f0306
choiitea <rickywjchoi@gmail.com>
2022-04-14 10:39:10 -0400

    DIR test with order rep

---
+++ OrderRepresentation/condition1_DIRtest.ex
@@ -0,0 +1,38 @@
+#Square Cylinder Triangle Diamond Circle Heart Cross Base Feature
+
+# Blue Triangle + Purple Diamond [SOC consistent]
+I:
+(ShapeONE)
+0 0 1 0 0 0 0 1 0
+(Color)
+0 0 0 0 1 0 0
+(ShapeTWO)
+0 0 0 1 0 0 0 0 1
+T:
+(ShapeONEOutput)
+0 0 1 0 0 0 0 1 0
+(ColorOutput)
+0 0 0 0 1 0 0
+(ShapeTWOOutput)
+0 0 0 1 0 0 0 0 1
+(Conclusion)
+1
+
+# Purple Diamond + Red Square [DIR Inconsistent]
+I:
+(ShapeONE)
+0 0 0 1 0 0 0 1 0
+(Color)
+1 0 0 0 0 0 0
+(ShapeTWO)
+1 0 0 0 0 0 0 0 1
+T:
+(ShapeONEOutput)
+0 0 0 1 0 0 0 1 0
+(ColorOutput)
+1 0 0 0 0 0 0
+(ShapeTWOOutput)
+1 0 0 0 0 0 0 0 1
+(Conclusion)
+1
+;
