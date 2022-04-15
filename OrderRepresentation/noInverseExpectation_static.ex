adeb21cc1e3cc946dcfd8affa83dddde828747dc
choiitea <rickywjchoi@gmail.com>
2022-04-14 20:16:33 -0400

    + diff levels of expec via frequency + Controlled Dir. Test

---
+++ OrderRepresentation/noInverseExpectation_static.ex
@@ -0,0 +1,39 @@
+defT:-
+
+#Red Square + Purple Diamond
+freq: 10
+name: "Red Square + Purple Diamond"
+I:
+(ShapeONE)
+1 0 0 0 0 0 0 1 0
+(Color)
+1 0 0 0 0 0 0
+(ShapeTWO)
+0 0 0 1 0 0 0 0 1
+T:
+(ShapeONEOutput)
+1 0 0 0 0 0 0 1 0
+(ColorOutput)
+1 0 0 0 0 0 0
+(ShapeTWOOutput)
+0 0 0 1 0 0 0 0 1
+;
+
+#Green Cylinder + Yellow Circle
+freq: 8
+name: "Green Cylinder + Yellow Circle"
+I:
+(ShapeONE)
+0 1 0 0 0 0 0 1 0
+(Color)
+0 0 1 0 0 0 0
+(ShapeTWO)
+0 0 0 0 1 0 0 0 1
+T:
+(ShapeONEOutput)
+0 1 0 0 0 0 0 1 0
+(ColorOutput)
+0 0 1 0 0 0 0
+(ShapeTWOOutput)
+0 0 0 0 1 0 0 0 1
+;
