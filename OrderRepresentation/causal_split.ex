39ebf5b2d30d2ac96a1fb7d6df493663bf0052a8
Deon T. Benton <dbenton2@swarthmore.edu>
2022-04-26 00:10:30 -0400

    added a causal_split.ex file

---
+++ OrderRepresentation/causal_split.ex
@@ -0,0 +1,48 @@
+defT:-
+#Red Square
+name: "Red Square"
+I:
+(ShapeONE)
+1 0 0 0 0 0 0
+T:
+(ShapeONEOutput)
+1 0 0 0 0 0 0
+(Conclusion)
+1
+;
+
+#Green Cylinder
+name: "Green Cylinder"
+I:
+(ShapeONE)
+0 1 0 0 0 0 0
+T:
+(ShapeONEOutput)
+0 1 0 0 0 0 0
+(Conclusion)
+0
+;
+
+#Purple Diamond
+name: "Purple Diamond"
+I:
+(ShapeONE)
+0 0 0 1 0 0 0
+T:
+(ShapeONEOutput)
+0 0 0 1 0 0 0
+(Conclusion)
+1
+;
+
+#Yellow Circle
+name: "Yellow Circle"
+I:
+(ShapeONE)
+0 0 0 0 1 0 0
+T:
+(ShapeONEOutput)
+0 0 0 0 1 0 0
+(Conclusion)
+0
+;
