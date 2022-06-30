f33dcffe76d0a03cf28679768e10dad26e248a69
Ricky Choi <rickywjchoi@gmail.com>
2022-06-29 23:46:36 -0400

    a very unsatisfactory temporary soln

---
+++ VariedExpectations (v1)/causal_tempSoln_strongInverse.ex
@@ -0,0 +1,25 @@
+defT:-
+
+#Red Square
+name: "Red Square"
+I:
+(ShapeTWO)
+1 0 0 0 0 0 0
+T:
+(ShapeTWOOutput)
+1 0 0 0 0 0 0
+(Conclusion)
+1
+;
+
+#Green Cylinder
+name: "Green Cylinder"
+I:
+(ShapeTWO)
+0 1 0 0 0 0 0
+T:
+(ShapeTWOOutput)
+0 1 0 0 0 0 0
+(Conclusion)
+0
+;
